686616f7048a128b66802b93d2a78aba651ea4da
DragonflyStats <kobriendublin@gmail.com>
2016-12-10 17:55:41 +0000

    Update 00-RelationalLogicalOperators.jl

--- 00-RelationalLogicalOperators.jl
+++ 00-RelationalLogicalOperators.jl
@@ -3,22 +3,24 @@
 ## As we saw earlier, when testing for equality we use ==
 
 x = 1
-1
 
 x == 2
-false
-For “not equal” use !=
+# false
 
+# For “not equal” use !=
 x != 3
-true
-We can chain inequalities:
+# true
+
+############################################################
+# We can chain inequalities:
 
 1 < 2 < 3
-true
+# true
 
 1 <= 2 <= 3
-true
-In many languages you can use integers or other values when testing conditions but Julia is more fussy
+# true
+############################################################
+# In many languages you can use integers or other values when testing conditions but Julia is more fussy
 
 while 0 println("foo") end
 ERROR: TypeError: non-boolean (Int64) used in boolean context
@@ -27,9 +29,9 @@ ERROR: TypeError: non-boolean (Int64) used in boolean context
 if 1 print("foo") end
 ERROR: TypeError: non-boolean (Int64) used in boolean context
 
-Combining Expressions
+# Combining Expressions
 
-Here are the standard logical connectives (conjunction, disjunction)
+# Here are the standard logical connectives (conjunction, disjunction)
 
 true && false
 false
@@ -38,5 +40,5 @@ true || false
 true
 Remember
 
-P && Q is true if both are true, otherwise it’s false
-P || Q is false if both are false, otherwise it’s true
+# P && Q is true if both are true, otherwise it’s false
+# P || Q is false if both are false, otherwise it’s true
